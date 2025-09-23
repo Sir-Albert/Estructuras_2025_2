1b9f9fbb2424986afa5a04d4606085a868cdb99a
SirAlbert <edgar.betancourt@uabc.edu.mx>
2025-09-23 14:43:47 -0700

    Arbol

--- src/practica5/Main.c
+++ src/practica5/Main.c
@@ -17,14 +17,28 @@ typedef struct
 }Caja;
 
 
-int generar_articulos(void);
+int* generar_articulos(void);
+void imprimirEntero(void*);
 
 int main(void)
 {
-	//Caja cajas[NUMERO_CAJAS];
+	Caja cajas[NUMERO_CAJAS];
+	
+	for(int i=0; i<NUMERO_CAJAS ; i++)
+	{
+		cajas[i].cola = inicializarCola();
+		cajas[i].cola.imprimir = &imprimirEntero;
+		cajas[i].cola.liberar = &free;
+	}
 	int opcion;	
 	do
 	{
+		
+		for(int i=0; i<NUMERO_CAJAS ; i++)
+		{
+			printf("\n Caja[%d]: ",i+1);
+			imprimirCola(cajas[i].cola);
+		}
 		opcion = 0;
 		//DESPLEGAR LAS CAJAS
 		printf("\n [1] AVANZAR TICK");
@@ -35,7 +49,13 @@ int main(void)
 		switch(opcion)
 		{
 			case 1:
-			printf("\n [1]");
+			for(int i=0; i<NUMERO_CAJAS ; i++)
+			{
+				//SI ESTA HABILITADA Y SI NO ESTA LLENA
+				//GENERAR Y FORMAR CLIENTE
+				if(cajas[i].estado && !llenaC(cajas[i].cola))
+					enqueue( &cajas[i].cola , generar_articulos() );
+			}
 			break;
 			case 2:
 			printf("\n [2]");
@@ -57,7 +77,17 @@ int main(void)
 }
 
 
-int generar_articulos(void)
+int* generar_articulos(void)
 {		
-	return rand() % (MAX_ARTICULOS - MIN_ARTICULOS + 1) + MIN_ARTICULOS;
+	int *articulos = malloc(sizeof(int));
+	*articulos = rand() % (MAX_ARTICULOS - MIN_ARTICULOS + 1) + MIN_ARTICULOS;
+	return articulos; 
+	
+}
+
+
+void imprimirEntero(void *a)
+{
+	int *aa=a;
+	printf("%d",*aa);
 }
